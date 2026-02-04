1131b6adbd036bf0e6c815c1c86818a9e28850c9
Tisha Jhabak <iamtisha22@gmail.com>
2026-02-04 12:17:20 +0530

    added sentry config

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 - [x] Add image upload
 - [x] "taint" (server-only) - just discussed
 - [x] Add Next/Image component
-- [ ] Error management (w/ Sentry)
+- [x] Error management (w/ Sentry)
 - [ ] Routing/image page (parallel route)
 - [ ] Delete button (w/ Server Actions)
 - [ ] Analytics (posthog)
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://41e317c8bdcefbf548a6a9a6b46e0cdc@o4510826206068736.ingest.de.sentry.io/4510826207641680",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
